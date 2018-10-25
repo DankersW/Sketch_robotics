2a929b4968a6984f000fa19fa490a0214b7e1add
Dankers Wouter <wouter.dankers@skynet.be>
2018-10-25 21:24:25 +0200

    Macros added

--- Sketch_Robotics_Arduino/Sketch_Robotics_Arduino.cpp
+++ Sketch_Robotics_Arduino/Sketch_Robotics_Arduino.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 
+
+#include "src/Macros.h"
 #include "src/InterruptHandlers.h"
 
 void setup() {
-
+	int test = DIRECTION_PIN;
 }
 
 void loop() {
---
+++ Sketch_Robotics_Arduino/src/Macros.h
@@ -0,0 +1,3 @@
+#define HALL_F_M1 2 // Digital Interrupt pin 2 --> INT0
+#define ENABLE_PIN 5
+#define DIRECTION_PIN 4
